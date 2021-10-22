0a4a0cdd2ad52f8556c6f4dc613b9c5fd57fbf6b
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2021-10-22 09:40:02 -0400

    test: updated tests for new abstraction

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,15 @@
 using SpecTools
-using Test
+using Test, Random
 
-@testset "Energy Levels" begin
-  # test the creation of the energy levels
-  @test LinearLevel(109.2, 1, 1, 0) isa LinearLevel
-  @test AsymTopLevel(5190256., 50, 20, 5, 3, 0) isa AsymTopLevel
+@testset "Base types" begin
+  Random.seed!(2151)
+  transitions = SpecTools.make_linear_levels(10) |> SpecTools.make_linear_transitions
+  @test match_encoding(transitions, [0, 0, 1, 0]) == 1
+  # test the matrix-ification of transitions
+  tf = features(transitions)
+  @test tf[:,1] == [2616.245527993644, 1.0, 0.0, 0.0, 1.0, 0.0] 
+  # test grabbing the upper state energy levels
+  @test upper_state_qnos(transitions[1]) == [1, 0]
 end
 
 @testset "Lineshapes" begin
