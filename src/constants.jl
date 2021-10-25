bf62dffaee46b1bda1fae76bd8576ff89140047f
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2021-10-25 09:24:12 -0400

    fix: what are signs anyway

--- src/constants.jl
+++ src/constants.jl
@@ -4,7 +4,7 @@ using PhysicalConstants.CODATA2018: c_0, h, k_B
 # Boltzmann constant in cm/K
 const k_cm = Float64((k_B * (1 / h) * (1 / (c_0 * 100))).val)
 
-const k_mhz = k_cm * (c_0.val * 1e4)
+const k_mhz = k_cm * (c_0.val / 1e4)
 # c, h, k_B = c_0.val, h.val, k_B.val
 
 export
