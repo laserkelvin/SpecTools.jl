65de27143564b36a568a4912d8def2b0a8e3c256
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2021-10-25 10:28:56 -0400

    style: making main module blue style compliant

--- src/SpecTools.jl
+++ src/SpecTools.jl
@@ -1,48 +1,34 @@
 module SpecTools
 
-using Parameters, Distributions, Interpolations
-using SHA, PhysicalConstants, MetaGraphs
+using Parameters
+using Distributions
+using Interpolations
+using SHA
+using PhysicalConstants
+using MetaGraphs
+using Tullio
 
 # Write your package code here.
 
 include("constants.jl")
 include("types.jl")
+
+export Level, Transition, Levels, Transitions
+export e, g, ν, I, quantum_numbers, features
+export match_encoding, make_linear_transitions, upper_state_qnos, lower_state_qnos
+
 include("graphs.jl")
 
-export Level,
-  Transition,
-  Levels,
-  Transitions,
-  e,
-  g,
-  ν,
-  I,
-  quantum_numbers,
-  features,
-  match_encoding,
-  make_linear_transitions,
-  upper_state_qnos,
-  lower_state_qnos
-
-export BipartiteSG,
-  ProjectedSG,
-  offset,
-  indices
-
-using Tullio
+export BipartiteSG, ProjectedSG, offset, indices
 
 include("lineshapes.jl")
 
-export MultiGaussian,
-  Gaussian,
-  simulate_lineshape
+export MultiGaussian, Gaussian, simulate_lineshape
 
 include("signal.jl")
 include("partition_function.jl")
 
-export partition_function,
-  read_qpart,
-  InterpPartitionFunction
+export partition_function, read_qpart, InterpPartitionFunction
 
 include("utils.jl")
 
