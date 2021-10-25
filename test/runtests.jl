7883ffba361fbc1a98304c50ca786f8671def0c4
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2021-10-25 10:31:21 -0400

    test: updating tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,11 @@ using Test, Random
 
 @testset "Base types" begin
   Random.seed!(2151)
-  transitions = SpecTools.make_linear_levels(10) |> SpecTools.make_linear_transitions
+  transitions = SpecTools.make_linear_levels(30) |> SpecTools.make_linear_transitions
   @test match_encoding(transitions, [0, 0, 1, 0]) == 1
   # test the matrix-ification of transitions
   tf = features(transitions)
-  @test tf[:,1] == [2616.245527993644, 1.0, 0.0, 0.0, 1.0, 0.0] 
+  @test tf[:,1] == [200.0, 1.0, 0.0, 0.0, 1.0, 0.0] 
   # test grabbing the upper state energy levels
   @test upper_state_qnos(transitions[1]) == [1, 0]
 end
@@ -15,6 +15,7 @@ end
 @testset "Graph types" begin
   levels = SpecTools.make_linear_levels(20)
   transitions = SpecTools.make_linear_transitions(levels)
+  # test constructing the graph objects
   proj_graph = ProjectedSG(levels, transitions)
   @test proj_graph isa SpecTools.ProjectedSG
   bi_graph = BipartiteSG(levels, transitions)
